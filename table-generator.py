6234ff17caf67216af1f6c0f1c6715924585c815
Thomas Faschang <thomasfaschang@mailbox.org>
2023-04-19 08:03:53 +0200

    renamed file and removed locks

--- table-generator.py
+++ table-generator.py
@@ -7,21 +7,22 @@ Created on Sat Jun 27 22:16:44 2020
 """
 
 from tkinter import filedialog
-from tkinter import *
+from tkinter import Tk
 import os
 import csv
 
 cwd = os.getcwd()
 root = Tk()
 
-filename =  filedialog.askopenfilename(initialdir = cwd, title = "Select file",filetypes = (("csv files","*.csv"),("all files","*.*")))
+filename = filedialog.askopenfilename(initialdir=cwd, title="Select file", filetypes=(
+    ("csv files", "*.csv"), ("all files", "*.*")))
 root.update()
-print (filename)
+print(filename)
 root.destroy()
 
 rows = []
 
-with open(filename, newline = '') as csvfile:
+with open(filename, newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in reader:
         rows.append(row[0].split(';'))
@@ -29,10 +30,10 @@ with open(filename, newline = '') as csvfile:
 
 n_rows = len(rows)
 n_cols = max([len(row) for row in rows])
-    
+
 out = "\\begin{table}[H]\n"
-out += "\\centering\n\\begin{tabular}{" 
-out += "|c" * n_cols 
+out += "\\centering\n\\begin{tabular}{"
+out += "|c" * n_cols
 out += "|}\n\\hline\n"
 out += "TODO & " * (n_cols-1) + "TODO \\\\ \\hline\n"
 for row in rows:
